fb89a81ff457223bb2e105fc889fe6367406d55f
webmfl <martinflazo@gmail.com>
2023-01-30 10:17:17 -0300

    cambio de localhost

--- src/app/services/autenticacion.service.ts
+++ src/app/services/autenticacion.service.ts
@@ -8,7 +8,7 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AutenticacionService {
-  url="http://localhost:8080/auth/login";
+  url="https://be-argpro.onrender.com/auth/login";
   currentUserSubject: BehaviorSubject<any>;
   constructor(private http:HttpClient) {
     console.log("El servicio de autenticacion esta corriendo...");
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,7 +10,7 @@ import { NuevoUsuario } from '../models/nuevo-usuario';
 })
 export class AuthService {
 
-  authURL = 'http://localhost:8080/auth/';
+  authURL = 'https://be-argpro.onrender.com/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
